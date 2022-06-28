d054a8aff0e9f1ae9f9c3c76c3f0b43bd3ce5fce
yokesh7373@gmail.com <gitHub@1>
2022-06-28 22:06:43 +0530

    Created SQLiteOpenHelper skeleton class.

---
+++ app/src/main/java/com/training/simplesqlite/EmployeeDbHelper.java
@@ -0,0 +1,28 @@
+package com.training.simplesqlite;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+public class EmployeeDbHelper extends SQLiteOpenHelper {
+
+    public EmployeeDbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
