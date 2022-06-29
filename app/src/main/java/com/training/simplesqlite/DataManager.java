61b02a6d718ea82b18b30a8afa07f73fc2adf91a
yokesh7373@gmail.com <gitHub@1>
2022-06-29 23:21:54 +0530

    Fetching Employee with id

--- app/src/main/java/com/training/simplesqlite/DataManager.java
+++ app/src/main/java/com/training/simplesqlite/DataManager.java
@@ -71,4 +71,55 @@ public class DataManager {
 
         return resultId;
     }
+
+    public static Employee fetchEmployee(EmployeeDbHelper employeeDbHelper, long empId) {
+        Log.d(TAG, "fetching Employee");
+        SQLiteDatabase sqLiteDb = employeeDbHelper.getReadableDatabase();
+        Employee employee = null;
+
+        String[] columns = {
+                EmployeeDbContract.EmployeeEntry.COLUMN_ID,
+                EmployeeDbContract.EmployeeEntry.COLUMN_NAME,
+                EmployeeDbContract.EmployeeEntry.COLUMN_DOB,
+                EmployeeDbContract.EmployeeEntry.COLUMN_DESIGNATION,
+        };
+
+        // Filter results WHERE "id" = '3'
+        String selection = EmployeeDbContract.EmployeeEntry.COLUMN_ID + " LIKE ? ";
+        String[] selectionArgs = {String.valueOf(empId)};
+
+        Cursor resultCursor = sqLiteDb.query(
+                EmployeeDbContract.EmployeeEntry.TABLE_NAME,
+                columns,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null);
+
+        int idPos = resultCursor.getColumnIndex(EmployeeDbContract.EmployeeEntry.COLUMN_ID);
+        int namePos = resultCursor.getColumnIndex(EmployeeDbContract.EmployeeEntry.COLUMN_NAME);
+        int dobPos = resultCursor.getColumnIndex(EmployeeDbContract.EmployeeEntry.COLUMN_DOB);
+        int designationPos = resultCursor.getColumnIndex(EmployeeDbContract.EmployeeEntry.COLUMN_DESIGNATION);
+
+        if (resultCursor.getCount() == 0) {
+            Log.d(TAG, empId + " id is not present in DB");
+        }
+        else if (resultCursor.getCount() > 1) {
+            Log.d(TAG, "There are employees matching with this id " + empId);
+        }
+        else {
+            while (resultCursor.moveToNext()) {
+                int id = resultCursor.getInt(idPos);
+                String name = resultCursor.getString(namePos);
+                Long dob = resultCursor.getLong(dobPos);
+                String designation = resultCursor.getString(designationPos);
+                employee = new Employee(id, name, dob, designation);
+            }
+        }
+        Log.d(TAG, "Employee: " + employee);
+        resultCursor.close();
+        return employee;
+
+    }
 }
