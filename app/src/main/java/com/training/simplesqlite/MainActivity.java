d5ef7fcfaf3f657b68bb5189d87dff87fc850023
yokesh7373@gmail.com <gitHub@1>
2022-06-29 19:32:59 +0530

    Created Intent to launch AddEmployeeActivity

--- app/src/main/java/com/training/simplesqlite/MainActivity.java
+++ app/src/main/java/com/training/simplesqlite/MainActivity.java
@@ -1,14 +1,37 @@
 package com.training.simplesqlite;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.fab);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addEmployeeIntent = new Intent(getBaseContext(), AddEmployeeActivity.class);
+                startActivityForResult(addEmployeeIntent, 1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
